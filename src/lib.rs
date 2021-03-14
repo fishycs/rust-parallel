4e3781f2bab574f25ef21ba5ecc7ccde680e5ca4
Ethan Fisher <ethan8@cs.washington.edu>
2021-03-14 04:25:39 +0100

    removed the need for prefix tree and zero trait

--- src/lib.rs
+++ src/lib.rs
@@ -8,24 +8,40 @@ use std::ops::AddAssign;
 const SEQUENTIAL_THRESHOLD: usize = 256;
 
 // parallel prefix sum (in place)
-pub fn parallel_prefix_sum<T: Send>(array: &mut[T]) where for<'t> T: AddAssign<&'t T> {
+pub fn parallel_prefix_sum<T: Send>(array: &mut [T])
+where
+    for<'t> T: AddAssign<&'t T>,
+{
     parallel_prefix_sum_tune(array, num_cpus::get(), SEQUENTIAL_THRESHOLD);
 }
 
 // parallel prefix sum (copy)
-pub fn parallel_prefix_sum_tune<T: Send>(array: &mut[T], cores: usize, threshold: usize) where for<'t> T: AddAssign<&'t T> {
-    parallel_prefix_tune(array, &|a: &mut T, b: &T| {
-	*a += b;
-    }, cores, threshold);
+pub fn parallel_prefix_sum_tune<T: Send>(array: &mut [T], cores: usize, threshold: usize)
+where
+    for<'t> T: AddAssign<&'t T>,
+{
+    parallel_prefix_tune(
+        array,
+        &|a: &mut T, b: &T| {
+            *a += b;
+        },
+        cores,
+        threshold,
+    );
 }
 
 // parallel prefix (in place)
-pub fn parallel_prefix<T: Send, F: Fn(&mut T, &T) + Sync>(array: &mut[T], accumulate_fn: &F) {
+pub fn parallel_prefix<T: Send, F: Fn(&mut T, &T) + Sync>(array: &mut [T], accumulate_fn: &F) {
     parallel_prefix_tune(array, accumulate_fn, num_cpus::get(), SEQUENTIAL_THRESHOLD);
 }
 
 // parallel prefix (copy)
-pub fn parallel_prefix_tune<T: Send, F: Fn(&mut T, &T) + Sync>(array: &mut[T], accumulate_fn: &F, cores: usize, threshold: usize) {
+pub fn parallel_prefix_tune<T: Send, F: Fn(&mut T, &T) + Sync>(
+    array: &mut [T],
+    accumulate_fn: &F,
+    cores: usize,
+    threshold: usize,
+) {
     // collect and build sums
     parallel_prefix_accumulate(array, accumulate_fn, cores, threshold);
 
@@ -35,67 +51,80 @@ pub fn parallel_prefix_tune<T: Send, F: Fn(&mut T, &T) + Sync>(array: &mut[T], a
 }
 
 // collect and build sums
-fn parallel_prefix_accumulate<T: Send, F: Fn(&mut T, &T) + Sync>(array: &mut[T], accumulate_fn: &F, cores: usize, threshold: usize) {
+fn parallel_prefix_accumulate<T: Send, F: Fn(&mut T, &T) + Sync>(
+    array: &mut [T],
+    accumulate_fn: &F,
+    cores: usize,
+    threshold: usize,
+) {
     if cores == 1 || array.len() <= threshold {
-	// sequential
-	let (array, end) = array.split_at_mut(array.len() - 1);
-	let sum: &mut T = &mut end[0];
-	for t in array {
-	    accumulate_fn(sum, t);
-	}
+        // sequential
+        let (array, end) = array.split_at_mut(array.len() - 1);
+        let sum: &mut T = &mut end[0];
+        for t in array {
+            accumulate_fn(sum, t);
+        }
     } else {
-	// parallel
-	let left_cores: usize = cores/2;
+        // parallel
+        let left_cores: usize = cores/2;
         let right_cores: usize = cores - left_cores;
 
         // have to split, since each thread requires unique access...
         let (left, right) = array.split_at_mut(array.len()/2);
-	
+
         crossbeam::scope(|scope| {
             scope.spawn(|_| {
                 parallel_prefix_accumulate(left, accumulate_fn, left_cores, threshold);
             });
             parallel_prefix_accumulate(right, accumulate_fn, right_cores, threshold);
-	}).unwrap(); // probably should handle this error somehow...
+        })
+        .unwrap(); // probably should handle this error somehow...
 
-	accumulate_fn(&mut right[right.len() - 1], &left[left.len() - 1]); // sum accumulation
+        accumulate_fn(&mut right[right.len() - 1], &left[left.len() - 1]); // sum accumulation
     }
 }
 
 // finalize and distribute sums
-fn parallel_prefix_distribute<T: Send, F: Fn(&mut T, &T) + Sync>(array: &mut[T], accumulate_fn: &F, cores: usize, threshold: usize, start: bool) {
+fn parallel_prefix_distribute<T: Send, F: Fn(&mut T, &T) + Sync>(
+    array: &mut [T],
+    accumulate_fn: &F,
+    cores: usize,
+    threshold: usize,
+    start: bool,
+) {
     if cores == 1 || array.len() <= threshold {
-	// sequential
-	for i in 1..array.len() {
-	    let (prev, now) = array.split_at_mut(i);
-	    accumulate_fn(&mut now[0], &prev[i - 1]);
-	}
+        // sequential
+        for i in 1..array.len() {
+            let (prev, now) = array.split_at_mut(i);
+            accumulate_fn(&mut now[0], &prev[i - 1]);
+        }
     } else {
-	// parallel
-	let left_cores: usize = cores/2;
+        // parallel
+        let left_cores: usize = cores/2;
         let right_cores: usize = cores - left_cores;
-	
-	let left;
-	let right;
-	
-	if start {
-	    let (left_array, right_array) = array.split_at_mut((array.len() + 1)/2 - 1);
-	    left = left_array;
-	    right = right_array;
-	} else {
-	    let (left_array, right_array) = array.split_at_mut(array.len()/2);
-	    left = left_array;
-	    right = right_array;
 
-	    accumulate_fn(&mut right[0], &left[0]); // from left accumulation
-	}
+        let left;
+        let right;
 
-	crossbeam::scope(|scope| {
-	    scope.spawn(|_| {
-		parallel_prefix_distribute(left, accumulate_fn, left_cores, threshold, start);
-	    });
-	    parallel_prefix_distribute(right, accumulate_fn, right_cores, threshold, false);
-	}).unwrap();
+        if start {
+            let (left_array, right_array) = array.split_at_mut((array.len() + 1)/2 - 1);
+            left = left_array;
+            right = right_array;
+        } else {
+            let (left_array, right_array) = array.split_at_mut(array.len()/2);
+            left = left_array;
+            right = right_array;
+
+            accumulate_fn(&mut right[0], &left[0]); // from left accumulation
+        }
+
+        crossbeam::scope(|scope| {
+            scope.spawn(|_| {
+                parallel_prefix_distribute(left, accumulate_fn, left_cores, threshold, start);
+            });
+            parallel_prefix_distribute(right, accumulate_fn, right_cores, threshold, false);
+        })
+        .unwrap();
     }
 }
 
@@ -142,7 +171,7 @@ mod tests {
         let dur_seq = start_seq.elapsed();
 
         // check integrity of results
-	assert_eq!(seq, par);
+        assert_eq!(seq, par);
 
         // print results
         println!("parallel = {:?}, sequential = {:?}", dur_par, dur_seq);
